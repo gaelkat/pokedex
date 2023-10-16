277f5725ec5edbbbb9f8888aafcf679b1f884dbb
KAT <143718151+gaelkat@users.noreply.github.com>
2023-10-16 11:37:27 +0200

    exo 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import PokemonCard from "./components/PokemonCard";
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   return (
     <>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,4 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
@@ -9,14 +8,3 @@ function PokemonCard() {
 }
 
 export default PokemonCard;
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
